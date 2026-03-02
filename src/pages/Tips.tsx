71db1a476d63158d9edda0b9cb0fd5268bb2ed35
jcf-junior <h1ddenntc@gmail.com>
2026-03-02 00:52:02 +0000

    fix: update projectUrl and credentialUrl to empty strings in initialCv.json; format Tips component for better readability

--- src/pages/Tips.tsx
+++ src/pages/Tips.tsx
@@ -1,3 +1,7 @@
 export default function Tips() {
-  return <div className="p-4 h-[80vh] text-2xl text-center">Nothing to see here, for now.</div>;
+  return (
+    <div className="p-4 h-[80vh] text-2xl text-center">
+      Nothing to see here, for now.
+    </div>
+  );
 }
