9cb26bd8cf816d9d3d446d4b708f53480589505b
jcf-junior <h1ddenntc@gmail.com>
2026-03-07 02:34:19 +0000

    feat: Add zoom functionality to resume preview in editor

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,9 +1,12 @@
 import EditorInputs from "../components/EditorInputs";
 import JakesResume from "../templates/JakesResume";
 import { useCvData } from "../hooks/useCvData";
+import { useState } from "react";
 
 export default function Editor() {
   const { cvData, setCvData } = useCvData();
+
+  const [scaleFactor, setScaleFactor] = useState(1);
 
   return (
     <div className="max-w-[1600px] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 lg:p-8">
@@ -19,6 +22,7 @@ export default function Editor() {
               A4 Size
             </span>
           </div>
+
           <div className="flex items-center gap-2">
             <button
               className="flex items-center gap-2 bg-slate-900 text-white px-6 py-2.5 rounded-xl font-bold hover:bg-slate-800 cursor-pointer transition-all shadow-md active:scale-95 disabled:opacity-50 disabled:cursor-not-allowed"
@@ -26,6 +30,7 @@ export default function Editor() {
             >
               <i className="fas fa-download"></i>Download PDF
             </button>
+
             <button
               className="flex items-center gap-2 bg-red-500 text-white px-6 py-2.5 rounded-xl font-bold hover:bg-slate-800 cursor-pointer transition-all shadow-md active:scale-95 disabled:opacity-50 disabled:cursor-not-allowed"
               onClick={() => {
@@ -38,7 +43,27 @@ export default function Editor() {
           </div>
         </div>
 
-        <div className="w-fit shadow">
+        <div className="w-[170px] bg-white flex items-center justify-center rounded-2xl gap-1 text-gray-100">
+          <button
+            className="rounded-l-2xl bg-slate-900 w-[50px]"
+            onClick={() => setScaleFactor((prev) => prev + 0.1)}
+          >
+            <i className="fas fa-magnifying-glass-plus"></i>
+          </button>
+          
+          <div className="text-black font-bold">{(scaleFactor * 100).toFixed(0)}%</div>
+
+          <button
+            className="rounded-r-2xl bg-slate-900 w-[50px]"
+            onClick={() => setScaleFactor((prev) => prev - 0.1)}
+          >
+            <i className="fas fa-magnifying-glass-minus"></i>
+          </button>
+        </div>
+        <div
+          className="w-fit shadow"
+          style={{ scale: `${scaleFactor}`, transformOrigin: "top left" }}
+        >
           <JakesResume cvData={cvData} />
         </div>
       </div>
