f54563e2cfb96515482de5ab73e17c5bcf3d666c
jcf-junior <h1ddenntc@gmail.com>
2026-03-07 03:48:58 +0000

    feat: Implemented zoom controls in the editor toolbar

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,6 +1,34 @@
-export default function Toolbar() {
+import type React from "react";
+
+type Props = {
+  scaleFactor: number;
+  setScaleFactor: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export default function Toolbar({ setScaleFactor, scaleFactor }: Props) {
   return (
     <div className="flex gap-5 justify-center items-center no-print">
+      <div className="bg-white flex items-center justify-center rounded-2xl gap-1 text-gray-100">
+        <button
+          className="flex gap-2 items-center px-5 py-2 rounded-l-2xl bg-slate-900 hover:bg-slate-800 hover:scale-105 cursor-pointer transition-all"
+          onClick={() => setScaleFactor((prev) => prev + 0.1)}
+        >
+          <i className="fas fa-magnifying-glass-plus"></i>
+          Zoom In
+        </button>
+
+        <div className="text-black font-bold">
+          {(scaleFactor * 100).toFixed(0)}%
+        </div>
+
+        <button
+          className="flex gap-2 items-center px-5 py-2 rounded-r-2xl bg-slate-900 hover:bg-slate-800 hover:scale-105 cursor-pointer transition-all"
+          onClick={() => setScaleFactor((prev) => prev - 0.1)}
+        >
+          <i className="fas fa-magnifying-glass-minus"></i>
+          Zoom Out
+        </button>
+      </div>
 
       <button
         className="flex items-center gap-2 bg-slate-900 text-white px-5 py-2 rounded-xl font-bold hover:bg-slate-800 hover:scale-105 cursor-pointer transition-all shadow-md active:scale-95 disabled:opacity-50 disabled:cursor-not-allowed"
--- src/pages/LivePreview.tsx
+++ src/pages/LivePreview.tsx
@@ -1,19 +1,20 @@
+import { useState } from "react";
 import JakesResume from "../templates/JakesResume";
 import { useCvData } from "../hooks/useCvData";
 import Toolbar from "../components/Toolbar";
 
 export default function LivePreview() {
-
+  const [scaleFactor, setScaleFactor] = useState(0.9);
   const { cvData } = useCvData();
 
   return (
     <>
-      <div className="p-2 flex flex-col gap-4">
-        <Toolbar />
+      <div className="sticky p-2 flex flex-col gap-4">
+        <Toolbar setScaleFactor={setScaleFactor} scaleFactor={scaleFactor} />
 
         <div
           className="w-fit shadow mx-auto "
-          
+          style={{ scale: `${scaleFactor}`, transformOrigin: "top center" }}
         >
           <JakesResume cvData={cvData} />
         </div>
