638d76a25ae7257e8552e4f52a22e1cbd9217e3c
jcf-junior <h1ddenntc@gmail.com>
2026-03-23 00:38:41 +0000

    fix: adjust Toolbar layout for improved spacing and consistency

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -17,7 +17,7 @@ export default function Toolbar({
 }: Props) {
   return (
     <div className="fixed bottom-10 mx-auto flex gap-5 w-fit justify-center items-center bg-white rounded-full px-2 py-2 border border-slate-200 shadow-lg z-9999">
-      <div className="bg-white flex items-center justify-center text-gray-100">
+      <div className="bg-white flex gap-1 items-center justify-center text-gray-100">
         <button
           className="text-slate-500 hover:text-slate-800 cursor-pointer transition-all"
           onClick={() => zoomOut()}
@@ -29,7 +29,7 @@ export default function Toolbar({
           key={scaleFactor}
           aria-label="Zoom percentage"
           defaultValue={(scaleFactor * 100).toFixed(0) + "%"}
-          className="w-[45px] text-slate-500 font-bold px-1 text-center focus:outline-none text-xs"
+          className="text-slate-500 w-[5ch] font-bold px-1 text-center focus:outline-none text-xs"
           onKeyDown={(e) => {
             if (e.key == "Enter") {
               const val = parseInt(e.currentTarget.value);
@@ -50,16 +50,18 @@ export default function Toolbar({
         >
           <i className="fas fa-plus"></i>
         </button>
-
-        <button
-          className="text-slate-500 hover:text-slate-800 cursor-pointer transition-all"
-          onClick={() => {
-            resetCv();
-          }}
-        >
-          <i className="fas fa-redo"></i>
-        </button>
       </div>
+
+      <span className="w-px h-4 bg-slate-300"></span>
+
+      <button
+        className="text-slate-500 hover:text-slate-800 cursor-pointer transition-all"
+        onClick={() => {
+          resetCv();
+        }}
+      >
+        <i className="fas fa-redo"></i>
+      </button>
 
       <button
         className="flex items-center gap-2 bg-indigo-600 text-white px-6 py-2 rounded-full font-bold hover:bg-slate-800 cursor-pointer transition-all shadow-md active:scale-95 disabled:opacity-50 disabled:cursor-not-allowed text-sm"
