e8b7f3706aeac8365fd236a6c5278d9d05d7dec3
jcf-junior <hello@jcfjunior.dev>
2026-04-03 20:54:24 +0100

    refactor: remove Tips page and associated route for cleaner navigation

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import App from "./App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Editor from "./pages/Editor.tsx";
 import Home from "./pages/Home.tsx";
-import Tips from "./pages/Tips.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -14,7 +13,6 @@ createRoot(document.getElementById("root")!).render(
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
           <Route path="/editor" element={<Editor />} />
-          <Route path="/tips" element={<Tips />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/Tips.tsx
+++
@@ -1,7 +0,0 @@
-export default function Tips() {
-  return (
-    <div className="p-4 h-[80vh] text-2xl text-center">
-      Nothing to see here, for now.
-    </div>
-  );
-}
